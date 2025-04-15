ebe2274f169a72a484c85199fc4a7b5ee87a92b0
arjunarora-17 <77168933+arjunarora-17@users.noreply.github.com>
2025-04-15 16:11:06 +0530

    added interpolation

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def train(args, teacher, student, generator, device, optimizer, epoch):
         optimizer_G.zero_grad()
         generator.train()
         fake = generator(z)
+        fake = F.interpolate(fake, size=(224, 224), mode='bilinear', align_corners=False)
         t_logit = teacher(fake) 
         s_logit = student(fake)
 
@@ -62,6 +63,8 @@ def test(args, student, generator, device, test_loader, epoch=0):
 
             z = torch.randn( (data.shape[0], args.nz, 1, 1), device=data.device, dtype=data.dtype )
             fake = generator(z)
+            fake = F.interpolate(fake, size=(224, 224), mode='bilinear', align_corners=False)
+
             output = student(data)
             if i==0:
                 input_image = pack_images(denormalize(data, (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)).clamp(0,1).detach().cpu().numpy())
@@ -154,7 +157,7 @@ def main():
 
     print("######################################")
 
-    generator = network.gan.GeneratorA(nz=args.nz, nc=3, img_size=224)
+    generator = network.gan.GeneratorA(nz=args.nz, nc=3, img_size=64)
 
     teacher.load_state_dict( torch.load( args.ckpt ) )
     print("Teacher restored from %s"%(args.ckpt))
