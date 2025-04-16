578b61fcfc977a7fbf8142c117e035f5c3e20a78
arjunarora-17 <77168933+arjunarora-17@users.noreply.github.com>
2025-04-16 23:06:13 +0530

    bug fix

--- network/gan.py
+++ network/gan.py
@@ -9,7 +9,7 @@ class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.shape[0], -1)
 
-class GeneratorA(nn.Module):
+class Generator(nn.Module):
     def __init__(self, nz=100, ngf=64, nc=1, img_size=32):
         super(GeneratorA, self).__init__()
 
@@ -43,60 +43,3 @@ class GeneratorA(nn.Module):
         img = self.conv_blocks2(img)
         return img
 
-
-class GeneratorB(nn.Module):
-    """ Generator from DCGAN: https://arxiv.org/abs/1511.06434
-    """
-    def __init__(self, nz=256, ngf=64, nc=3, img_size=64, slope=0.2):
-        super(GeneratorB, self).__init__()
-        if isinstance(img_size, (list, tuple)):
-            self.init_size = ( img_size[0]//16, img_size[1]//16 )
-        else:    
-            self.init_size = ( img_size // 16, img_size // 16)
-
-        self.project = nn.Sequential(
-            Flatten(),
-            nn.Linear(nz, ngf*8*self.init_size[0]*self.init_size[1]),
-        )
-
-        self.main = nn.Sequential(
-            nn.BatchNorm2d(ngf*8),
-            
-            nn.ConvTranspose2d(ngf*8, ngf*4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf*4),
-            nn.LeakyReLU(slope, inplace=True),
-            # 2x
-
-            nn.ConvTranspose2d(ngf*4, ngf*2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf*2),
-            nn.LeakyReLU(slope, inplace=True),
-            # 4x
-            
-            nn.ConvTranspose2d(ngf*2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
-            nn.LeakyReLU(slope, inplace=True),
-            # 8x
-
-            nn.ConvTranspose2d(ngf, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
-            nn.LeakyReLU(slope, inplace=True),
-            # 16x
-
-            nn.Conv2d(ngf, nc, 3,1,1),
-            nn.Tanh(),
-        )
-
-        for m in self.modules():
-            if isinstance(m, (nn.ConvTranspose2d, nn.Linear, nn.Conv2d)):
-                nn.init.normal_(m.weight, 0.0, 0.02)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            if isinstance(m, (nn.BatchNorm2d)):
-                nn.init.normal_(m.weight, 1.0, 0.02)
-                nn.init.constant_(m.bias, 0)
-        
-    def forward(self, z):
-        proj = self.project(z)
-        proj = proj.view(proj.shape[0], -1, self.init_size[0], self.init_size[1])
-        output = self.main(proj)
-        return output
